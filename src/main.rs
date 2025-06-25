f5a2af20188c85f40c932b017f3cde5852dc170a
anonymous <206280993+UnwindSafe@users.noreply.github.com>
2025-06-25 18:05:22 +0200

    updated main to read example code and lex.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use lexer::Lexer;
+
+mod lexer;
+
 fn main() {
+    // get the code for our example program.
+    let code = include_str!("../examples/test.tml");
+
+    let tokens = Lexer::new(code).scan();
+
+    println!("{:?}", tokens);
+
     println!("Hello, world!");
 }
