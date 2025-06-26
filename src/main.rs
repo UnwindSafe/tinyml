986886679465babac7b437dda11927cffa3239eb
anonymous <206280993+UnwindSafe@users.noreply.github.com>
2025-06-26 13:52:57 +0200

    added logger and thiserror

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ use lexer::Lexer;
 mod lexer;
 
 fn main() {
+    // set the environment variable so that it always prints logs, maybe a better way.
+    unsafe { std::env::set_var("RUST_LOG", "trace") };
+
+    pretty_env_logger::init();
+
     // get the code for our example program.
     let code = include_str!("../examples/test.tml");
 
