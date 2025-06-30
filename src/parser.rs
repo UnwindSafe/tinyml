7a1397362b11b3756276572a4c78b3e2fa0d4ecd
anonymous <206280993+UnwindSafe@users.noreply.github.com>
2025-06-30 16:03:06 +0200

    added let expression to parser.

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,16 @@
 use log::error;
 
-use crate::lexer::Token;
+use crate::lexer::{Lexeme, LexemeKind, Token};
+
+#[derive(Debug)]
+pub enum Expr {
+    Let {
+        ident: Token,
+        eq_expr: Box<Expr>,
+        in_expr: Box<Expr>,
+    },
+    Literal(Token),
+}
 
 pub struct Parser {
     tokens: Vec<Token>,
@@ -29,14 +39,16 @@ impl Parser {
     }
 
     /// Peek, `n`, tokens ahead. By default it peeks a single token ahead.
-    fn peek(&self, n: Option<usize>) -> Option<Token> {
-        self.tokens.get(self.pointer + n.unwrap_or(1)).cloned()
+    fn peek(&self, n: Option<isize>) -> Option<Token> {
+        self.tokens
+            .get((self.pointer as isize + n.unwrap_or(1)) as usize)
+            .cloned()
     }
 
     /// Check to see if the next token matches the provided token.
-    fn is_match(&self, token: Token) -> bool {
+    fn is_match(&self, lexeme: LexemeKind) -> bool {
         if let Some(t) = self.peek(None) {
-            if t == token {
+            if t.lexeme.kind() == lexeme {
                 return true;
             }
         }
@@ -44,8 +56,8 @@ impl Parser {
     }
 
     // If matched consumes the target token.
-    fn accept(&mut self, token: Token) -> bool {
-        if self.is_match(token) {
+    fn accept(&mut self, lexeme: LexemeKind) -> bool {
+        if self.is_match(lexeme) {
             self.advance();
             true
         } else {
@@ -53,15 +65,57 @@ impl Parser {
         }
     }
 
+    /// Like `accept` but matches multiple tokens, returning the lexme it matched.
+    fn accept_lexemes(&mut self, lexemes: impl Iterator<Item = LexemeKind>) -> Option<LexemeKind> {
+        for lexeme in lexemes {
+            if self.accept(lexeme.clone()) {
+                return Some(lexeme);
+            }
+        }
+        None
+    }
+
+    /// Gets the token before the current token.
+    fn previous(&self) -> Option<Token> {
+        self.peek(Some(-1))
+    }
+
     // Must match and consume this token otherwise error.
-    fn expect(&mut self, token: Token) {
-        if !self.accept(token.clone()) {
+    fn expect(&mut self, lexeme: LexemeKind) -> Result<Token, ()> {
+        if !self.accept(lexeme) {
             self.errored = true;
             error!(
                 "expected symbol: {:?}, found {:?}.",
-                token,
+                lexeme,
                 self.current_token(),
             );
+            return Err(());
         }
+
+        Ok(self.previous().unwrap())
+    }
+
+    fn expr(&mut self) -> Result<Expr, ()> {
+        // "let" <ident> "=" <expr> "in" <expr> "end".
+        if self.accept(LexemeKind::LET) {
+            let ident = self.expect(LexemeKind::IDENTIFIER).unwrap();
+            self.expect(LexemeKind::EQ)?;
+            let eq_expr = self.expr()?;
+            self.expect(LexemeKind::IN)?;
+            let in_expr = self.expr()?;
+            self.expect(LexemeKind::END)?;
+
+            return Ok(Expr::Let {
+                ident,
+                eq_expr: Box::new(eq_expr),
+                in_expr: Box::new(in_expr),
+            });
+        }
+
+        Err(())
+    }
+
+    pub fn parse(&mut self) -> Expr {
+        self.expr().unwrap()
     }
 }
